431e5c26eafac10c12c760e40dd53067c1b257f1
NajewskiMichal <147168133+NajewskiMichal@users.noreply.github.com>
2023-10-13 16:15:13 +0200

    Add files via upload

---
+++ ConsoleApp1/Rectangle2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Rectangle
+    {
+        public double a, b;
+
+        public Rectangle(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public double CalculateRectangleArea(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ ConsoleApp1/RectangleMain2.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp1;
+
+namespace trojkat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b;
+            Console.Write("Podaj pierwszy bok : ");
+            while (!double.TryParse(Console.ReadLine(), out a) || a < 0)
+            {
+                Console.Write("Niepoprawna wartość");
+                Thread.Sleep(240);
+                Console.Write(",\nWprowadź poprawną wartość boku 1 : ");
+            }
+            Console.Write("Podaj drugi bok : ");
+            while (!double.TryParse(Console.ReadLine(), out b) || b < 0)
+            {
+                Console.Write("Niepoprawna wartość");
+                Thread.Sleep(240);
+                Console.Write(",\nWprowadź poprawną wartość boku 2 : ");
+            }
+            Rectangle rectangle = new Rectangle(a,b);
+
+            Console.Clear();
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write($"Pole prostokąta o bokach {a}, {b} wynosi ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{rectangle.CalculateRectangleArea(a, b)} cm²");
+            Console.ResetColor();
+            Console.OutputEncoding = System.Text.Encoding.Default;
+        }
+
+    }
+}
