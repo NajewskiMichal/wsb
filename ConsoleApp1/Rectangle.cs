c931da7bc5a275a30233b193b38f59b970c433eb
NajewskiMichal <147168133+NajewskiMichal@users.noreply.github.com>
2023-10-13 16:02:21 +0200

    Add files via upload

---
+++ ConsoleApp1/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Rectangle
+    {
+        public double a;
+        public double b;
+        
+
+        public static double CalculateArea(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ ConsoleApp1/RectangleMain.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp1;
+
+namespace trojkat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            Console.Write("Podaj pierwszy bok : ");
+            while (!double.TryParse(Console.ReadLine(), out rectangle.a) || rectangle.a < 0)
+            {
+                Console.Write("Niepoprawna wartość");
+                Thread.Sleep(240);
+                Console.Write(",\nWprowadź poprawną wartość boku 1 : ");
+            }
+            Console.Write("Podaj drugi bok : ");
+            while (!double.TryParse(Console.ReadLine(), out rectangle.b) || rectangle.b < 0)
+            {
+                Console.Write("Niepoprawna wartość");
+                Thread.Sleep(240);
+                Console.Write(",\nWprowadź poprawną wartość boku 2 : ");
+            }
+            double area = rectangle.a * rectangle.b;
+            Console.Clear();
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write($"Pole prostokąta o bokach {rectangle.a}, {rectangle.b} wynosi ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{area:F5}cm²");
+            Console.ResetColor();
+            Console.OutputEncoding = System.Text.Encoding.Default;
+        }
+
+    }
+}
