7fede7402707b7c74911ec0388d93a30fbd2a44b
NajewskiMichal <147168133+NajewskiMichal@users.noreply.github.com>
2023-10-13 15:21:33 +0200

    Add files via upload

---
+++ ConsoleApp1/PersonMain.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp1;
+
+namespace trojkat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.firstName = "Janusz";
+            person.lastName = "Nowak";
+            person.weight = 90.5F;
+            person.height = 192;
+            Console.WriteLine($"Dane użytownika : {person.getData()}");
+
+            Person person2 = new Person();
+            person2.firstName = "Janusz";
+            person2.lastName = "Kowal";
+            person2.weight = 70.5F;
+            person2.height = 192;
+            Console.WriteLine($"Dane użytownika : {person.getData()}");
+
+        }
+    }
+}
